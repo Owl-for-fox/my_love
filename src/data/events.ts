85e2c74c52c3b8a9ba886abbe9d07f749d5b21e4
Chekin <chechekin2009@gmail.com>
2026-06-19 22:10:45 +0300

    event

--- src/data/events.ts
+++ src/data/events.ts
@@ -47,5 +47,5 @@ export const events: readonly DayEvent[] = [
   {date: '2026-06-17', text: 'Ты'},
   {date: '2026-06-18', text: 'Чудо'},
   {date: '2026-06-19', text: 'Ляжки такие вкусняшки...'},
-  {date: '2026-06-20', text: 'Ты'},
+  {date: '2026-06-20', text: 'Мяу'},
 ]
